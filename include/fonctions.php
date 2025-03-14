67e93eba123bec85e38a76f2ea5858e8a98c53df
lenabct <lena.bechet73@gmail.com>
2025-03-14 15:09:00 +0100

    ajout jean

---
+++ include/fonctions.php
@@ -0,0 +1,12 @@
+<?php
+
+    function ajout_jean($conn,$nom,$type,$taille,$prix){
+        $sql = "INSERT INTO Vente (`nom`, `type`, 'taille', 'prix') VALUE ('$nom', '$type', $taille, $prix)";
+        global $debeug;
+        if ($debeug)
+            echo $sql;
+        $res = mysqli_query($conn, $sql);
+        return $res;
+    }
+
+?>
