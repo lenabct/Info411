1c564f0d21d39c35a7dafb32516d19953c487034
lenabct <lena.bechet73@gmail.com>
2025-03-14 15:00:08 +0100

    oui

---
+++ formulaire.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form id="ajoutForm" class="form" >
+        <h2>Ajouter un Jean</h2>
+        <input type="Nom" id="nomVendeur" placeholder="Nom" required /> <br>
+        <input type="Type" id="typeJean" placeholder="Type du Jean" required /> <br>
+        <input type="Taille" id="tailleJean" placeholder="Taille du Jean" required /> <br>
+        <input type="Prix" id="prixJean" placeholder="Prix du Jean" required /> <br>
+        <button type="submit">Ajouter</button>
+    </form>
+    
+</body>
+</html>
