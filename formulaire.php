c27f7d9a798f830076c6fd08ef0d47ceb5023d9e
lenabct <lena.bechet73@gmail.com>
2025-03-28 08:04:31 +0100

    oui

--- formulaire.php
+++ formulaire.php
@@ -14,34 +14,31 @@
         <form methode="POST" action="./formulaire.php">
             <div class ="row">
                 <div class="element">
-                    <input placeholder="Nom" type="text" name="nom"/> <br>
-                    <input placeholder="Type" type="text" name="type"/> <br>
-                    <input placeholder="Taille" type="int" name="taille"/> <br>
-                    <input placeholder="Prix" type="int" name="prix"/> <br>
+                    <input placeholder="Nom" type="text" name="nom"> <br>
+                    <input placeholder="Type" type="text" name="type"> <br>
+                    <input placeholder="Taille" type="int" name="taille"> <br>
+                    <input placeholder="Prix" type="int" name="prix"> <br>
                 </div>
             </div>
             <input type="submit" value="Ajouter" class="button"> <br>
         </form>
 
         <?php
-            include ("include/connexion.php");
-            include ("include/fonctions.php");
+        include ("include/connexion.php");
+        include ("include/fonctions.php");
 
-            if (!empty($_POST["nom"])) {
+        if (!empty($_POST["nom"])) {
 
-                $nom = $_POST["nom"];
-                $type = $_POST["type"];
-                $taille = $_POST["taille"];
-                $prix = $_POST["prix"];
+            $nom = $_POST["nom"];
+            $type = $_POST["type"];
+            $taille = $_POST["taille"];
+            $prix = $_POST["prix"];
 
-                console.log($nom);
+            console.log($nom);
 
-                ajout_jean($conn,$nom,$type,$taille,$prix);
-            }
+            ajout_jean($conn,$nom,$type,$taille,$prix);
+        }
         ?>
-    </div>
-
-        
-    
+    </div>   
 </body>
 </html>
